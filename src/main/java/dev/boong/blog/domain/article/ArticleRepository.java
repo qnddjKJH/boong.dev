4df55b7a86849a7f47647dcd6db267869c8fda7b
JongHun Kim(boongDev) <qnddjKJH.dev@gmail.com>
2022-09-26 00:25:46 +0900

    add: Domains Repository

---
+++ src/main/java/dev/boong/blog/domain/article/ArticleRepository.java
@@ -0,0 +1,7 @@
+package dev.boong.blog.domain.article;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
---
+++ src/main/java/dev/boong/blog/domain/hashtag/HashtagRepository.java
@@ -0,0 +1,6 @@
+package dev.boong.blog.domain.hashtag;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
+}
